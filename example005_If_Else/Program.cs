89e2c89dab69453e0d116352505c0332ebf2e979
CJINBNH <alex_egorov_89@mail.ru>
2022-08-03 20:38:58 +0400

    added example005_If_Else

---
+++ example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя ");
+string userName = Console.ReadLine();
+if (userName == "Маша")
+{
+    Console.WriteLine ("Ура, это же Маша, привет!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
